91e4fb43cadf155aa6cab29f05ca8f0703dd404f
Christian Autermann <c.autermann@52north.org>
2016-05-18 14:30:16 +0200

    fixed import script

--- index.js
+++ index.js
@@ -7,29 +7,5 @@ var execute = require('./lib/execute');
 var mongodb = 'mongodb://localhost:27017/enviroCar';
 var postgres = 'postgres://postgres:postgres@localhost:5432/enviroCar';
 var query = {};
-/*
-copy(mongodb, postgres, query, function(err) {
-  if (err) {
-    console.error(err);
-  } else {
-    var sqlScript = path.join(__dirname, 'lib', 'postprocess.sql');
-    execute(postprocess, sqlScript, function(err, result) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(result);
-      }
-    });
-  }
-});
-*/
 
-
-var postprocess = path.join(__dirname, 'lib', 'postprocess.sql');
-    execute(postgres, postprocess, function(err, result) {
-      if (err) {
-        console.warn("Fehler:" + err);
-      } else {
-        console.log(result);
-      }
-    });
+copy(mongodb, postgres, query, console.error.bind(console));
