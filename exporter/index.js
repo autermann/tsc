85a26c1e3ba7a59a90dd84d08b0f9516bcf5dbd3
Christian Autermann <c.autermann@52north.org>
2016-07-04 09:48:56 +0200

    switched to lowercase database name

--- exporter/index.js
+++ exporter/index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var copy = require('./lib/copy');
 var execute = require('./lib/execute');
 var mongodb = 'mongodb://localhost:27017/enviroCar';
-var postgres = 'postgres://postgres:postgres@localhost:5432/enviroCar';
+var postgres = 'postgres://postgres:postgres@localhost:5432/envirocar';
 var query = {};
 
 copy(mongodb, postgres, query, console.error.bind(console));
