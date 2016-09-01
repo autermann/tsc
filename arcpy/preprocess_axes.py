4a8bd72972b0cc62b597b0234dfa5764e653f290
Christian Autermann <c.autermann@52north.org>
2016-09-01 13:00:13 +0200

    add length and duration fields to axis segments

--- arcpy/preprocess_axes.py
+++ arcpy/preprocess_axes.py
@@ -1,5 +1,6 @@
 import textwrap
 from config import axis_model, setenv
+from arcpy import Polyline, Array, PointGeometry
 
 def add_rank_field(model, field_name):
     if self.field_exist(field_name):
@@ -25,7 +26,33 @@ def add_rank_field(model, field_name):
     finally:
         fl.delete()
 
+
+def add_length_fields(model):
+    fc = model.segments
+    fc.add_field('length', 'DOUBLE')
+    fc.add_field('blength', 'DOUBLE')
+    fc.add_field('dlength', 'DOUBLE')
+    fc.add_field('duration', 'DOUBLE')
+    fc.add_field('bduration', 'DOUBLE')
+    fc.add_field('dduration', 'DOUBLE')
+
+
+    with fc.update(['SHAPE@', 'length', 'blength', 'dlength', 'duration', 'bduration', 'dduration']) as rows:
+        for row in rows:
+            shape = row[0]
+            array = Array([shape.firstPoint, shape.lastPoint])
+            polyline = Polyline(array, shape.spatialReference)
+            row[1] = shape.getLength('GEODESIC', 'METERS')
+            row[2] = polyline.getLength('GEODESIC', 'METERS')
+            row[3] = abs(row[1]-row[2])
+            row[4] = row[1] / (50/3.6)
+            row[5] = row[2] / (50/3.6)
+            row[6] = abs(row[4]-row[5])
+
+            rows.updateRow(row)
+
 if __name__ == '__main__':
     setenv()
-    axis_model.segments.add_id_field('segment_id')
-    add_rank_field(axis_model, 'S_Rang')
+    #axis_model.segments.add_id_field('segment_id')
+    #add_rank_field(axis_model, 'S_Rang')
+    add_length_fields(axis_model)
