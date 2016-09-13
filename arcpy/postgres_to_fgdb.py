89564cdd255baeac9979d1421b87691949312ecc
Christian Autermann <c.autermann@52north.org>
2016-09-13 09:57:38 +0200

    refactoring

--- arcpy/postgres_to_fgdb.py
+++ arcpy/postgres_to_fgdb.py
@@ -1,11 +1,15 @@
 import arcpy
 import os
 
-tracks_in=r'Database Connections\envirocar@localhost.sde\envirocar.public.tracks'
-measurements_in=r'Database Connections\envirocar@localhost.sde\envirocar.public.measurements'
-trajectories_in=r'Database Connections\envirocar@localhost.sde\envirocar.public.trajectories'
+tracks_in = r'Database Connections\envirocar@localhost.sde\envirocar.public.tracks'
+measurements_in = r'Database Connections\envirocar@localhost.sde\envirocar.public.measurements'
+trajectories_in = r'Database Connections\envirocar@localhost.sde\envirocar.public.trajectories'
 
 out_fgdb=r'C:\tsc\envirocar_new.gdb'
+
+tracks_out = os.path.join(out_fgdb, 'tracks')
+measurements_out = os.path.join(out_fgdb, 'measurements')
+trajectories_out = os.path.join(out_fgdb, 'trajectories')
 
 arcpy.conversion.FeatureClassToFeatureClass(in_features=trajectories_in, out_path=out_fgdb, out_name='trajectories')
 arcpy.conversion.FeatureClassToFeatureClass(in_features=measurements_in, out_path=out_fgdb, out_name='measurements')
@@ -17,10 +21,10 @@ arcpy.conversion.FeatureClassToFeatureClass(in_features=tracks_in,       out_pat
 	track "track" true true false 50 Long 0 0 ,First,#,{tracks_in},id,-1,-1
 	""".format(tracks_in=tracks_in))
 
-arcpy.management.AddIndex(in_table=os.path.join(out_fgdb, 'measurements'), fields=['track'], index_name='track_idx')
-arcpy.management.AddIndex(in_table=os.path.join(out_fgdb, 'measurements'), fields=['time'], index_name='time_idx')
-arcpy.management.AddIndex(in_table=os.path.join(out_fgdb, 'measurements'), fields=['time', 'track'], index_name='time_track_idx')
-arcpy.management.AddIndex(in_table=os.path.join(out_fgdb, 'tracks'),       fields=['track'], index_name='track_idx')
-arcpy.management.AddIndex(in_table=os.path.join(out_fgdb, 'trajectories'), fields=['track'], index_name='track_idx')
-arcpy.management.AddIndex(in_table=os.path.join(out_fgdb, 'trajectories'), fields=['end_time'], index_name='end_time_idx')
-arcpy.management.AddIndex(in_table=os.path.join(out_fgdb, 'trajectories'), fields=['start_time'], index_name='start_time_idx')
+arcpy.management.AddIndex(in_table=measurements_out, fields=['track'],         index_name='track_idx')
+arcpy.management.AddIndex(in_table=measurements_out, fields=['time'],          index_name='time_idx')
+arcpy.management.AddIndex(in_table=measurements_out, fields=['time', 'track'], index_name='time_track_idx')
+arcpy.management.AddIndex(in_table=tracks_out,       fields=['track'],         index_name='track_idx')
+arcpy.management.AddIndex(in_table=trajectories_out, fields=['track'],         index_name='track_idx')
+arcpy.management.AddIndex(in_table=trajectories_out, fields=['end_time'],      index_name='end_time_idx')
+arcpy.management.AddIndex(in_table=trajectories_out, fields=['start_time'],    index_name='start_time_idx')
