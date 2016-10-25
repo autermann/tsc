05cd5d315e3e1697afa4b9e81ce9ccbdc80cfc07
Christian Autermann <c.autermann@52north.org>
2016-10-25 11:49:41 +0200

    added debugging statements

--- arcpy/calculate_statistics.py
+++ arcpy/calculate_statistics.py
@@ -2,6 +2,9 @@ from config import fgdb as fgdb_all, setenv, axis_model, workspace
 from ec import calculate_statistics, create_tracks, create_stop_table, find_passages, create_result_tables
 from ooarcpy import FileGDB
 import os
+import logging
+
+log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     setenv()
@@ -12,15 +15,20 @@ if __name__ == '__main__':
 
 
     for fgdb in fgdbs:
+        log.debug('calculating statistics for %s', fgdb.id)
         measurements = fgdb.feature_class('measurements')
         stops = fgdb.table('stops')
         try:
             tracks = fgdb.feature_class('tracks')
+            log.debug('creating tracks')
             create_tracks(measurements, tracks)
+            log.debug('creating stop table')
             create_stop_table(measurements, stops)
+            log.debug('calculating statistics')
             calculate_statistics(axis_model, fgdb)
+            log.debug('finding passages')
             find_passages(fgdb, axis_model)
         finally:
             stops.delete_if_exists()
-
+        log.debug('creating result tables')
         create_result_tables(fgdb, axis_model)
