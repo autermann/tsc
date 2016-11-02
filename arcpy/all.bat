255b3f6435109a42f5df703f66d0f9f08e8844cd
Christian Autermann <c.autermann@52north.org>
2016-11-02 09:58:40 +0100

    stop batch file if a step fails

--- arcpy/all.bat
+++ arcpy/all.bat
@@ -1,6 +1,12 @@
 
-c:\Python27\ArcGIS10.3\python.exe c:\tsc\arcpy/split_into_weeks.py
-c:\Python27\ArcGIS10.3\python.exe c:\tsc\arcpy/calculate_statistics.py
-c:\Python27\ArcGIS10.3\python.exe c:\tsc\arcpy/to_cest.py
+c:\Python27\ArcGIS10.3\python.exe c:\tsc\arcpy/preprocess_axes.py || goto :error
+c:\Python27\ArcGIS10.3\python.exe c:\tsc\arcpy/create_subsets.py || goto :error
+c:\Python27\ArcGIS10.3\python.exe c:\tsc\arcpy/split_into_weeks.py || goto :error
+c:\Python27\ArcGIS10.3\python.exe c:\tsc\arcpy/calculate_statistics.py || goto :error
+c:\Python27\ArcGIS10.3\python.exe c:\tsc\arcpy/to_cest.py || goto :error
 
-pause
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
